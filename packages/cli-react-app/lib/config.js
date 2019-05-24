d81c2ba525ca94278598f5e8eb531f77d931acf8
ong <ongw20@outlook.com>
2019-05-24 22:18:33 +0800

    Fix clone template auth issue

--- packages/cli-react-app/lib/config.js
+++ packages/cli-react-app/lib/config.js
@@ -2,7 +2,7 @@ const os = require('os')
 const path = require('path')
 
 module.exports = {
-  getTemplateUrl: (template) => `ongw20/cli-react#template-${template}`,
+  getTemplateUrl: (template) => `direct:https://github.com/ongw20/cli-react.git#template-${template}`,
   TMP_DIR: path.join(os.tmpdir(), 'cli-react'),
   VALID_TEMPLATES: [
     'babel',
