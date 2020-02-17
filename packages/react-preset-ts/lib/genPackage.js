c9d131e9e067be9133ba5791cc04e15db527a7dd
ong <ongw20@outlook.com>
2020-02-17 23:21:42 +0800

    Update preset ts deps

--- packages/react-preset-ts/lib/genPackage.js
+++ packages/react-preset-ts/lib/genPackage.js
@@ -11,43 +11,43 @@ function getCombinedJson(pkgJson) {
       test: 'jest --coverage'
     },
     devDependencies: {
-      '@babel/core': '^7.4.3',
-      '@babel/plugin-transform-runtime': '^7.4.3',
-      '@babel/preset-env': '^7.4.3',
-      '@babel/preset-react': '^7.0.0',
-      '@types/jest': '^24.0.13',
-      '@types/react': '^16.8.14',
-      '@types/react-dom': '^16.8.4',
-      '@typescript-eslint/eslint-plugin': '^2.0.0',
-      '@typescript-eslint/parser': '^1.7.0',
-      autoprefixer: '^9.6.0',
-      'babel-loader': '^8.0.5',
-      'clean-webpack-plugin': '^2.0.1',
-      'cli-react-service': '^0.6.0',
-      'copy-webpack-plugin': '^5.0.2',
-      'css-loader': '^2.1.1',
-      eslint: '^6.5.1',
-      'eslint-plugin-react': '^7.14.3',
+      '@babel/core': '^7.8.4',
+      '@babel/plugin-transform-runtime': '^7.8.3',
+      '@babel/preset-env': '^7.8.4',
+      '@babel/preset-react': '^7.8.3',
+      '@types/jest': '^25.1.2',
+      '@types/react': '^16.9.19',
+      '@types/react-dom': '^16.9.5',
+      '@typescript-eslint/eslint-plugin': '^2.19.2',
+      '@typescript-eslint/parser': '^2.19.2',
+      autoprefixer: '^9.7.4',
+      'babel-loader': '^8.0.6',
+      'clean-webpack-plugin': '^3.0.0',
+      'cli-react-service': '^0.6.3',
+      'copy-webpack-plugin': '^5.1.1',
+      'css-loader': '^3.4.2',
+      eslint: '^6.8.0',
+      'eslint-plugin-react': '^7.18.3',
       'html-webpack-plugin': '^3.2.0',
-      husky: '^4.0.10',
-      jest: '^24.7.1',
-      less: '^3.10.3',
+      husky: '^4.2.3',
+      jest: '^25.1.0',
+      less: '^3.11.1',
       'less-loader': '^5.0.0',
-      'lint-staged': '^9.5.0',
-      'mini-css-extract-plugin': '^0.6.0',
+      'lint-staged': '^10.0.7',
+      'mini-css-extract-plugin': '^0.9.0',
       'postcss-loader': '^3.0.0',
-      'style-loader': '^0.23.1',
-      stylelint: '^10.1.0',
-      'stylelint-config-standard': '^18.3.0',
-      'ts-jest': '^24.0.2',
-      'ts-loader': '^5.4.3',
-      typescript: '^3.4.5'
+      'style-loader': '^1.1.3',
+      stylelint: '^13.2.0',
+      'stylelint-config-standard': '^20.0.0',
+      'ts-jest': '^25.2.0',
+      'ts-loader': '^6.2.1',
+      typescript: '^3.7.5'
     },
     dependencies: {
-      '@babel/runtime': '^7.4.3',
-      'core-js': '^3.1.3',
-      react: '^16.8.6',
-      'react-dom': '^16.8.6'
+      '@babel/runtime': '^7.8.4',
+      'core-js': '^3.6.4',
+      react: '^16.12.0',
+      'react-dom': '^16.12.0'
     },
     husky: {
       hooks: {
@@ -56,12 +56,10 @@ function getCombinedJson(pkgJson) {
     },
     'lint-staged': {
       '*.ts?(x)': [
-        'eslint --fix',
-        'git add'
+        'eslint --fix'
       ],
       '*.less': [
-        'stylelint --syntax=less --fix',
-        'git add'
+        'stylelint --syntax=less --fix'
       ]
     },
     browserslist: [
